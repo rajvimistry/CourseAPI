2c4043d0305f013fad39a12c64cf910547261499
Rajvi Mistry <rajvimistry93@gmail.com>
2018-08-13 10:41:23 -0400

    Spring Boot, Apache Derby, JPA

---
+++ CourseAPI/src/main/java/io/springbootstarter/CourseApiApp.java
@@ -0,0 +1,13 @@
+package io.springbootstarter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiApp.class, args);
+	}
+}
---
+++ CourseAPI/src/main/java/io/springbootstarter/courses/CourseService.java
@@ -0,0 +1,45 @@
+package io.springbootstarter.courses;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+	
+	@Autowired
+	private CourseRepository courseRepository;
+	
+	/*private List<Topic> topics = new ArrayList(Arrays.asList(
+			new Topic("spring", "Spring Framework", "Spring Framework Description"),
+			new Topic("angular", "Angular JS", "Angular JS Description"),
+			new Topic("java", "Core Java", "Core Java Description"),
+			new Topic("javascript", "JavaScript", "JavaScript Description")
+			)); */
+
+	public List<Course> getAllCourses(String topicId){
+		List<Course> courses = new ArrayList<>();
+		courseRepository.findByTopicId(topicId).forEach(courses::add);
+		return courses;
+	}
+	
+	public Course getCourse(String id) {
+		//return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		return courseRepository.findOne(id);
+	}
+
+	public void addCourse(Course course) {
+		courseRepository.save(course);	
+	}
+
+	public void updateCourse(Course course) {
+		courseRepository.save(course);		
+	}
+
+	public void deleteCourse(String id) {
+		courseRepository.delete(id);
+	}
+}
